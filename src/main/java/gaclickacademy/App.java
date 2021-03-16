7d17caa52df55664072823e2639d201661da3a4b
daniela <danieladurelli@hotmail.com>
2021-03-16 15:37:25 +0100

    gitx

--- src/main/java/gaclickacademy/App.java
+++ src/main/java/gaclickacademy/App.java
@@ -8,14 +8,8 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
-        System.out.println( "Hello World!" );
-<<<<<<< HEAD
-        System.out.println( "Hello World2!" );
-        System.out.println( "Hello World3!" );
-=======
-        System.out.println( "Hello Worvvvv" );
-        System.out.println( "Hello Worzzzz" );
->>>>>>> 8bb480937901c12f2a3687ac2689663fbfe938fd
+        System.out.println( "Hello Worldgitx!" );
+        System.out.println( "Hello World!gitx" );
+
     }
 }
